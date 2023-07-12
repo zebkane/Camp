d9844483ddac74431f1d727f8077882604292d03
zebkane <zebkane0@gmail.com>
2023-07-12 15:03:46 -0400

    added mBot code

--- MBot/destroyer_robot/destroyer_robot.ino
+++ MBot/destroyer_robot/destroyer_robot.ino
@@ -3,7 +3,12 @@
 MeDCMotor motor1(M1);
 MeDCMotor motor2(M2);
 
-MeUltrasonicSensor ultraSensor(PORT_3);
+MeUltrasonicSensor ultraSensor1(PORT_3);
+MeUltrasonicSensor ultraSensor2(PORT_4);
+MeLineFollower linefollower_1(PORT_1);
+MeLineFollower linefollower_2(PORT_2);
+MeRGBLed rgbled_1(7, 2);
+MeBuzzer buzzer;
 
 uint8_t motorSpeed = 255;
 
@@ -18,8 +23,8 @@ void drive(bool forward, int speed)
   }
   else
   {
-    motor1.run(-speed);
-    motor2.run(speed);
+    motor1.run(speed);
+    motor2.run(-speed);
   }
 }
 
@@ -27,6 +32,18 @@ void stop()
 {
   motor1.stop();
   motor2.stop();
+}
+
+void reactToLine() {  
+  if (linefollower_1.readSensors() != 3) {
+    drive(false, 255);
+    delay(1000);
+  }
+
+  if (linefollower_2.readSensors() != 3) {
+    drive(true, 255);
+    delay(1000);
+  }
 }
 
 void look(int speed)
@@ -40,9 +57,14 @@ void look(int speed)
 
 void charge(int speed)
 {
-  if (ultraSensor.distanceCm() <= 70)
+  if (ultraSensor1.distanceCm() <= 65)
   {
     drive(true, speed);
+    targetFound = true;
+  }
+  else if (ultraSensor2.distanceCm() <= 65)
+  {
+    drive(false, speed);
     targetFound = true;
   }
   else
@@ -52,13 +74,41 @@ void charge(int speed)
   }
 }
 
+void randomChance() {
+  if (!targetFound) {
+    int chance = random(1, 20);
+
+  if (chance <= 5) {
+    motor1.run(255);
+    motor2.run(255);
+    delay(1000);
+  } else if (chance <= 10) {
+    motor1.run(-255);
+    motor2.run(-255);
+    delay(1000);
+  }
+  }
+}
+
 void setup()
 {
   Serial.begin(9600);
+
+  rgbled_1.setColor(0, 255, 0, 0);
+  rgbled_1.show();
+
+  buzzer.tone(293.66, 0.25 * 1000);
+  delay(50);
+  buzzer.tone(293.66, 0.25 * 1000);
+  delay(50);
+  buzzer.tone(293.66, 0.25 * 1000);
+  delay(50);
 }
 
 void loop()
 {
+  reactToLine();
+
   look(255);
 
   charge(255);
